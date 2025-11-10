deef6ff206a2e36065d144e9fd30367ce611abbd
Andezion <245122@edu.p.lodz.pl>
2025-11-10 13:29:31 +0100

    traslated consts

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -7,21 +7,21 @@ class AppConstants {
   static const int dailyTaskBonus = 50;
   static const int reportProblemBonus = 10;
 
-  static const String activityWalking = 'Пешком';
-  static const String activityRunning = 'Бег';
-  static const String activityBicycle = 'Велосипед';
-  static const String activityScooter = 'Самокат';
+  static const String activityWalking = 'Walking';
+  static const String activityRunning = 'Run';
+  static const String activityBicycle = 'Bicycle';
+  static const String activityScooter = 'Scooter';
 
   static const int pointsPerLevel = 1000;
   static const int maxLevel = 100;
 
   static const List<String> drawingTemplates = [
-    'Сердце',
-    'Звезда',
-    'Смайлик',
-    'Единорог',
-    'Кошка',
-    'Какашка'
+      'Heart',
+      'Star',
+      'Smiley face',
+      'Unicorn',
+      'Cat',
+      'Poop'
   ];
 
   static const double maxDistanceKm = 50.0;
